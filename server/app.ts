68b81bde2f2921ddb2b19a2c9d6c4e1acda8b9d4
jelliott2021 <elliott.joh@northeastern.edu>
2025-01-19 21:03:10 -0500

    Updated cors to more requests

--- server/app.ts
+++ server/app.ts
@@ -95,7 +95,13 @@ process.on('SIGINT', () => {
 app.use(
   cors({
     credentials: true,
-    origin: [CLIENT_URL],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
   }),
 );
 
